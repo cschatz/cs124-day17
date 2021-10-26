68f58afa85b33333e463b1766a6d47a2d4d8bcc8
Colin Schatz <cschatz@mills.edu>
2021-10-26 10:24:56 -0700

    Update

--- src/BST.java
+++ src/BST.java
@@ -5,6 +5,13 @@ public class BST<E> {
 		E key;
 		BSTNode left;
 		BSTNode right;
+		
+		// Constructor
+		public BSTNode(E key) {
+			this.key = key;
+			left = null;
+			right = null;
+		}
 	}
 	
 	// The BST class as a whole has only one
@@ -18,6 +25,10 @@ public class BST<E> {
 	
 	public boolean isEmpty() {
 		return (root == null);
+	}
+	
+	public void insert(E item) {
+		BSTNode node = new BSTNode(item);
 	}
 	
 }
